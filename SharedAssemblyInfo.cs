c3a4f872e7f6a4410c67fef38875b677b80cb58e
mgoeppner <mgoeppner@gmail.com>
2022-01-11 20:26:57 +0100

    Update to 4.1.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.20.0")]
-[assembly: AssemblyFileVersion("4.0.20.4986")]
-[assembly: AssemblyInformationalVersion("4.0.20")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyInformationalVersion("4.1.0")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
--- SharedAssemblyInfo.template.cs
+++ SharedAssemblyInfo.template.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.20.0")]
-[assembly: AssemblyFileVersion("4.0.20.$REVNUM$")]
-[assembly: AssemblyInformationalVersion("4.0.20")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.$REVNUM$")]
+[assembly: AssemblyInformationalVersion("4.1.0")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
