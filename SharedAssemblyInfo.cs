2a2d2f970de686b61e89087acd1b725b6b1606d2
mgoeppner <mgoeppner@gmail.com>
2022-01-12 19:12:44 +0100

    Prepare to release 4.1.1

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EVEMon Development Team")]
 [assembly: AssemblyProduct("EVEMon")]
-[assembly: AssemblyCopyright("Copyright © 2006-2021, EVEMon Development Team")]
+[assembly: AssemblyCopyright("Copyright © 2006-2022, EVEMon Development Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.1")]
-[assembly: AssemblyFileVersion("4.1.0.1")]
-[assembly: AssemblyInformationalVersion("4.1.0")]
+[assembly: AssemblyVersion("4.1.1.1")]
+[assembly: AssemblyFileVersion("4.1.1.4995")]
+[assembly: AssemblyInformationalVersion("4.1.1")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
--- SharedAssemblyInfo.template.cs
+++ SharedAssemblyInfo.template.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.1")]
-[assembly: AssemblyFileVersion("4.1.0.$REVNUM$")]
-[assembly: AssemblyInformationalVersion("4.1.0")]
+[assembly: AssemblyVersion("4.1.1.1")]
+[assembly: AssemblyFileVersion("4.1.1.$REVNUM$")]
+[assembly: AssemblyInformationalVersion("4.1.1")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
