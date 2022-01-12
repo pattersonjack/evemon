c898d96a3a1d54ab6ed999b2faf22ef7ceddadfc
mgoeppner <mgoeppner@gmail.com>
2022-01-12 13:34:29 +0100

    Fix revision being 0
    The revision being set to 0 breaks settings files.
    
    See line 350 of .\evemon\src\EVEMon.Common\Helpers\PlanIOHelper.cs for the offender.
    
    Fixes #5

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.1.0.1")]
+[assembly: AssemblyFileVersion("4.1.0.1")]
 [assembly: AssemblyInformationalVersion("4.1.0")]
 
 // Neutral Language
