2536e71ca9528156d8da8e56cd8bf810f675080b
mgoeppner <mgoeppner@gmail.com>
2024-06-30 13:14:33 +0200

    Fix issue with assembly file version breaking settings load

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.1")]
 [assembly: AssemblyInformationalVersion("5.0.0")]
 
 // Neutral Language
